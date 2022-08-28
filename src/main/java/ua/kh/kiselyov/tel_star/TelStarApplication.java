f1508411e921fce9663d1f53cf6a8339f94d6238
Alexander Kiselyov <alexhhd01m@gmail.com>
2022-08-28 13:50:42 +0300

    Spring Boot/WebFlux app skeleton.

---
+++ src/main/java/ua/kh/kiselyov/tel_star/TelStarApplication.java
@@ -0,0 +1,13 @@
+package ua.kh.kiselyov.tel_star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelStarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/kh/kiselyov/tel_star/TelStarApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kh.kiselyov.tel_star;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelStarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
